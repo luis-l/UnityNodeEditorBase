0c8bac6687765ccf78285f347f0dba29674b1235
Luis Lopez <luislpz978@rocketmail.com>
2017-08-05 01:31:32 -0500

    DeleteNode handles connection reconstruction and deletion for undo and redo

--- NodeEditor/Assets/Editor/Actions/DeleteNodeAction.cs
+++ NodeEditor/Assets/Editor/Actions/DeleteNodeAction.cs
@@ -1,11 +1,15 @@
 ﻿
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DeleteNodeAction : UndoableAction
 {
     private NodeCanvas _canvas;
     private EditorNode _nodeRemoved = null;
+
+    private EditorOutputKnob _outputKnobToInput;
+    private List<EditorInputKnob> _inputKnobsToOutput;
 
     public override bool Init()
     {
@@ -17,15 +21,29 @@ public class DeleteNodeAction : UndoableAction
         _canvas = manager.window.canvas;
         _nodeRemoved = manager.window.state.selectedNode;
         _canvas.Remove(_nodeRemoved);
+
+        _outputKnobToInput = _nodeRemoved.input.OutputConnection;
+        _inputKnobsToOutput = _nodeRemoved.output.Inputs.ToList();
+
+        _outputKnobToInput.Remove(_nodeRemoved.input);
+        _nodeRemoved.output.RemoveAll();
     }
 
     public override void Undo()
     {
         _canvas.nodes.Add(_nodeRemoved);
+        _outputKnobToInput.Add(_nodeRemoved.input);
+
+        foreach (EditorInputKnob input in _inputKnobsToOutput) {
+            _nodeRemoved.output.Add(input);
+        }
     }
 
     public override void Redo()
     {
         _canvas.Remove(_nodeRemoved);
+
+        _outputKnobToInput.Remove(_nodeRemoved.input);
+        _nodeRemoved.output.RemoveAll();
     }
 }
